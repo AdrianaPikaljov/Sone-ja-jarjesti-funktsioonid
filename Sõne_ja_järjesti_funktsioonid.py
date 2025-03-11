c53927a6af6e91251aa9d2ae681965c18edf3841
adri <pikaljovadri@gmail.com>
2025-03-11 09:38:01 +0200

    Update Sõne_ja_järjesti_funktsioonid.py

--- Sõne_ja_järjesti_funktsioonid.py
+++ Sõne_ja_järjesti_funktsioonid.py
@@ -1,22 +1,70 @@
-
-while True:
-    print("Menüü:")
-    print("1. Pööra string")
-    print("2. Arvuta stringi pikkus")
-    print("3. Arvuta eseme esinemisajad listis")
-    print("4. Eemalda duplikaadid listist")
-    print("5. Sorteeri list")
-    print("6. Ühenda kaks stringi")
-    print("7. Jagage string eraldajaga")
-    print("8. Kas string koosneb ainult numbritest?")
-    print("9. Kas string koosneb ainult tähtedest?")
-    print("10. Lisa element listi")
-    print("0. Välju programm")
-    valik = input("Vali valik (0-10): ")
-
-    if valik == "1":
-        s = input("Sisesta string pööramiseks: ")
-        print(f"Pööratud string: {s[::-1]}")
+    while True:
+        print("menüü:")
+        print("1 - teisenda väiketähtedeks")
+        print("2 - teisenda suurtähtedeks")
+        print("3 - kontrolli, kas sisaldab ainult numbreid")
+        print("4 - jaga string eraldaja järgi")
+        print("5 - asenda alamsõne")
+        print("6 - otsi alamsõne (viimane esinemine)")
+        print("7 - otsi alamsõne (esimene esinemine)")
+        print("8 - leia stringi pikkus")
+        print("9 - korda stringi")
+        print("10 - uhenda stringid")
+        print("11 - muuda esitähed suur- ja väiketähtedeks")
+        print("12 - eemalda tühikud algusest ja lõpust")
+        print("13 - kontrolli, kas string algab kindla alamsõnaga")
+        print("0 - välju")
+    
+        valik = input("sisesta valik: ")
+    
+        if valik == "0":
+            break
+        elif valik == "1":
+            s = input("sisesta string: ")
+            print("vaiketähed:", s.lower()) # Teisendab kõik tähed väiketähtedeks
         elif valik == "2":
-            s = input("Sisesta string pikkuse arvutamiseks: ")
-            print(f"Stringi pikkus: {len(s)}")
+            s = input("sisesta string: ")
+            print("suurtähed:", s.upper())
+        elif valik == "3":
+            s = input("sisesta string: ")
+            print("kas string sisaldab ainult numbreid?", s.isdigit())
+        elif valik == "4":
+            s = input("sisesta string: ")
+            eraldaja = input("sisesta eraldaja: ")
+            print("stringi jagamine:", s.split(eraldaja))
+        elif valik == "5":
+            s = input("sisesta string: ")
+            vana = input("sisesta asendatav alamsõne: ")
+            uus = input("sisesta uus alamsõne: ")
+            print("alamsõne asendamine:", s.replace(vana, uus))
+        elif valik == "6":
+            s = input("sisesta string: ")
+            alamsoone = input("sisesta alamsõne: ")
+            print("viimase esinemise indeks:", s.rfind(alamsoone))
+        elif valik == "7":
+            s = input("sisesta string: ")
+            alamsoone = input("sisesta alamsõne: ")
+            print("esimese esinemise indeks:", s.find(alamsoone))
+        elif valik == "8":
+            s = input("sisesta string: ")
+            print("stringi pikkus: ", len(s))
+        elif valik == "9":
+            s = input("sisesta string: ")
+            kordus = int(input("mitu korda korrata? "))
+            print("stringi kordamine:", s * kordus)
+        elif valik == "10":
+            s1 = input("sisesta esimene string: ")
+            s2 = input("sisesta teine string: ")
+            print("stringide ühendamine: ", s1 + s2)
+        elif valik == "11":
+            s = input("sisesta string: ")
+            print("esitähtede muutmine suur- ja väiketähtedeks: ", s.title())
+        elif valik == "12":
+            s = input("sisesta string: ")
+            print("tuhikute eemaldamine: ", s.strip())
+        elif valik == "13":
+            s = input("sisesta string: ")
+            algus = input("sisesta kontrollitav algus: ")
+            print("kas string algab alamsõnaga?", s.startswith(algus))
+        else:
+            print("vale valik, proovi uuesti")
